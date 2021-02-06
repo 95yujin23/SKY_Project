9186f0ea555bb59537b73d1afba5b63451c0fc7b
ghana <ghana@LAPTOP-IERBKSF3>
2021-02-06 16:11:58 +0900

    hihihi Te

--- src/main/java/org/zerock/controller/HomeController.java
+++ src/main/java/org/zerock/controller/HomeController.java
@@ -11,6 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+//hihihi
+
+
 /**
  * Handles requests for the application home page.
  */
